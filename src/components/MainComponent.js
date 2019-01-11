c7b0d1d8e12b241139df45c807886c40198aaec1
Aman Prajapat <amandineshprajapat@gmail.com>
2019-01-11 14:05:27 +0530

    Reacr Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
+// import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom'; 
 
 class Main extends Component {
 
@@ -13,21 +14,20 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+ 
 
   render() {
     return (
       <div>
         <Header />     
-        <Menu dishes={this.state.dishes} 
-        onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+          <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={() => <Menu dishes= {this.state.dishes} />}/>
+            <Redirect to="/home"/>
+          </Switch>
         <Footer />
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,7 +6,6 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 function RenderMenuItem({ dish, onClick }) {
 	return (
 		<Card key={dish.id}
-			onClick={() => this.props.onClick(dish.id)}>
 			<CardImg width="100%" src={dish.image} alt={dish.name} />
 			<CardImgOverlay>
 				<CardTitle>{dish.name}</CardTitle>
