3676f23c05b9df44a14994b0920c5b30e7635bad
Aman Prajapat <amandineshprajapat@gmail.com>
2019-01-29 17:48:09 +0530

    Fetch Handling Errors

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardTitle, CardBody, CardText, CardImg, CardImgOverlay, Breadcrum
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -117,7 +118,7 @@ function RenderDish({ dish }) {
         return (
             // <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
